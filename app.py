cf859fee67563819fa0462094ffe73692f58e5f8
veerenchawda03 <68459900+veerenchawda03@users.noreply.github.com>
2023-04-12 19:11:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ todos = db.todo #Select the collection
 
 app = Flask(__name__)
 title = "TODO with Flask"
-heading = "ToDo Reminder PPT DEMO 3"
+heading = "ToDo Reminder PPT DEMO 4"
 #modify=ObjectId()
 
 def redirect_url():
